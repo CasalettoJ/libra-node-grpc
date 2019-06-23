113d8b875e03ee1df9a10d3bab892cd38e1af784
Jake <jake.m.casaletto@gmail.com>
2019-06-23 06:39:13 -0700

    before flow typing

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ TODOs:
 - Readme
 - flow typing
 - libraclient grpc implementation
+
+references from https://github.com/bonustrack/libra-grpc for basic npm grpc/protobuf handling and guidance on how to use admission control.
--- src/libraclient.js
+++ src/libraclient.js
@@ -4,7 +4,17 @@ import protoloader from "@grpc/proto-loader";
 
 const admission_control_proto = path.resolve(__dirname, "proto/admission_control.proto");
 
-class LibraClient {
+
+// todo: better control than const strings here
+// defined in get_with_proof.proto message RequestItem
+export const ClientRequests = {
+    accountState: "get_account_state_request",
+    accountTxBySequence: "get_account_transaction_by_sequence_number_request",
+    eventsByEventAccessPath: "get_events_by_event_access_path_request",
+    tx: "get_transactions_request"
+};
+
+export default class LibraClient {
     constructor(address) {
         this.serverAddress = address;
         const admissionControlDef = protoloader.loadSync(admission_control_proto, {
