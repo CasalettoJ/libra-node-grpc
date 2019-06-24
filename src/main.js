62045172dad0eb3441f6a0acf11d516641bec129
Jake <jake.m.casaletto@gmail.com>
2019-06-23 17:38:56 -0700

    improve testfile output

--- src/main.js
+++ src/main.js
@@ -4,9 +4,20 @@ const LibraClient = require("./libraclient");
 const testnetAddr = "ac.testnet.libra.org:8000";
 
 (() => {
-    console.log("creating libra grpc client");
-    const client = new LibraClient(testnetAddr);
-    console.log("client created");
-    console.log("testing UpdateToLatestLedger");
-    client.UpdateToLatestLedger([], (err, response) => console.log(response));
+  console.log("creating libra grpc client");
+  const client = new LibraClient(testnetAddr);
+  console.log("client created");
+  console.log("testing UpdateToLatestLedger");
+  client.UpdateToLatestLedger([], handleUpdateToLatestLedger);
 })();
+
+function handleUpdateToLatestLedger(err, response) {
+  console.log("Empty UpdateToLatestLedger call complete.");
+  if (err) {
+    console.log("Error!");
+    console.log(err);
+  } else {
+    console.log("Response:");
+    console.log(response);
+  }
+}
